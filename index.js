8d58adc27ad3aadeeb73a8c5d46ebc5c3868a0bd
hasanfuad <hasanmdfuad@hotmail.com>
2021-09-09 14:27:24 +0600

    server setup and connected with mongodb

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 27017;
+
+require('dotenv').config()
+
+app.use(cors);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shttn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const productCollection = client.db("emajohnStore").collection("products");
+
+client.connect(err => {
+   
+app.post('/addProducts', (req, res) => {
+    const product = req.body;
+    productCollection.insertOne(product)
+    .then(result => {
+        console.log("Data inserted", result);
+    })
+})
+});
+
+
+
+
+
+
+//default code
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
